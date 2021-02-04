48674c03cd4f7c12b50fcacd9289906c5d073c08
herve <09ariegeois@gmail.com>
2021-02-04 16:01:15 +0100

    modif blocs.js mis en commnetaire derniere ligne--remis page contact suite erreur manip

--- js/blocs.js
+++ js/blocs.js
@@ -199,5 +199,5 @@ $(document).ready(function() {
 }).resize(function() {
     setFillScreenBlocHeight()
 }), $(function() {
-    $('[data-toggle="tooltip"]').tooltip()
+    /*$('[data-toggle="tooltip"]').tooltip()*/
 });
